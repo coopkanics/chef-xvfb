775ae614736b2fc172ef5a976041ff0a77000841
Eric Tang <eric.x.tang@gmail.com>
2014-06-09 18:35:19 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'tech@clvrbot.com'
 license          'All rights reserved'
 description      'Installs/Configures xvfb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
